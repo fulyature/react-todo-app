f61804da058a44ceabbeaeba165b60b5c3de8aa4
fulya_ture <fulyature@gmail.com>
2023-07-16 15:54:02 +0300

    setState,keyboards events

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [todo, setTodo] = useState([]);
-  const [value, setValue] = useState(null);
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
+  const handleAdd = () => {
+    const newTodo = {
+      val: value,
+      id: todos.length,
+    };
+
+    setTodos([...todos, newTodo]);
+    setValue("");
+  };
+
   return (
     <div className="App">
       <div className="todo-app">
-        <input onChange={(e) => setValue(e.target.value)} />
+        <input value={value} onChange={(e) => setValue(e.target.value)} />
         <button onClick={handleAdd}>ToDo Add</button>
+      </div>
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <span>{todo.val}</span>
+            <button
+              onClick={() =>
+                setTodos((prev) => prev.filter((item) => item.id !== todo.id))
+              }
+            >
+              Delete
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
